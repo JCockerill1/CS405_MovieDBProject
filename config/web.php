a45fc0eb93604560bd59c9b41e6437912084a3f7
Joshua Cockerill <Jsco224@g.uky.edu>
2017-03-24 13:24:32 -0400

    Cookie Validation Key

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'UKY CS405 Spring 2017 MovieDBProject',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
